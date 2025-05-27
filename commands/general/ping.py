82c3dcb0414db17b7bf7dcbaae3919f32b445868
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-05-27 16:07:53 +0200

    Create ping.py

---
+++ commands/general/ping.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        embed = discord.Embed(
+            title="🏓 Pong !",
+            description=f"Latence du bot : **{latency}ms**",
+            color=discord.Color.green()
+        )
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
